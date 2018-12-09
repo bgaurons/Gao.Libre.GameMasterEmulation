7a9b02afbfda20a43437807bdc6baa8f5a4625c7
Benjamin Gauronskas <benjamin.gauronskas@gmail.com>
2018-12-09 12:27:09 -0700

    Factored out database code into separate project.

--- Gao.Database/ApplicationDbContext.cs
+++ Gao.Database/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Gao.Web.Data
+namespace Gao.Database
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- Gao.Database/Migrations/00000000000000_CreateIdentitySchema.cs
+++ Gao.Database/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace Gao.Web.Data.Migrations
+namespace Gao.Database.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
