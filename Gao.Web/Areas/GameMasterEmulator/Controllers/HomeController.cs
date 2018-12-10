b162b566698727d317bd8026f60d499f815c24d0
Benjamin Gauronskas <benjamin.gauronskas@gmail.com>
2018-12-10 14:40:58 -0700

    Added logic to spit out random stuff on a page that is not accessible without logging in first.

---
+++ Gao.Web/Areas/GameMasterEmulator/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gao.Libre.GameMasterEmulation.Mechanic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gao.Web.Areas.GameMasterEmulator.Controllers
+{
+    [Area("GameMasterEmulator")]
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var mission = MissionFactory.GenerateMission();
+            return View(mission);
+        }
+    }
+}
--- Gao.Web/Startup.cs
+++ Gao.Web/Startup.cs
@@ -78,8 +78,13 @@ namespace Gao.Web
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "area",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    );
+                routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                    );
             });
         }
     }
